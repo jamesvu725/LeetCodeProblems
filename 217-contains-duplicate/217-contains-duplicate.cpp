322a375b5e9f27292b41f531847c9ed9bdfddfd7
jamesvu725 <35977136+jamesvu725@users.noreply.github.com>
2022-06-22 14:34:26 -0700

    Time: 122 ms (64.50%), Space: 51.4 MB (50.90%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // if nums is 1 or 0, then there is no duplicates
-        if (nums.size() <= 1) {
-            return false;
-        }
         // use unordered set to store numbers
         unordered_set<int> hash;
         // iterate through the vector
         for (int i : nums) {
-            // if number already exists in the unordered set, return true
-            if (hash.count(i)) {
+            // insert number into hash
+            // insert returns a pair with the second element containing a bool
+            // if bool is false, then number already exists in hash
+            // return true if number already exists
+            if (!hash.insert(i).second) {
                 return true;
             }
-            // insert number into unordered set
-            hash.insert(i);
         }
         // if there are no duplicates
         return false;
