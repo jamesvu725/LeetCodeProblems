63e392ef70434ae2dfd3145370f25afefda89f47
jamesvu725 <35977136+jamesvu725@users.noreply.github.com>
2022-06-22 16:51:35 -0700

    Time: 38 ms (23.65%), Space: 18.1 MB (18.89%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -3,13 +3,27 @@ public:
     int missingNumber(vector<int>& nums) {
         // naive approach
         // sort and find first number
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+        // for (int i = 0; i < nums.size(); ++i) {
+        //     if (nums[i] != i) {
+        //         return i;
+        //     }
+        // }
+        // return size since all numbers are present between 0 and n-1
+        // return nums.size();
+        
+        // with extra memory
+        // create a boolean vector of size + 1
+        vector<bool> v(nums.size() + 1, false);
+        // flip the boolean value for each number in nums
         for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] != i) {
+            v[nums[i]] = true;
+        }
+        for (int i = 0; i < v.size(); ++i) {
+            if (v[i] == false) {
                 return i;
             }
         }
-        // return size since all numbers are present between 0 and n-1
-        return nums.size();
+        return -1;
     }
 };
