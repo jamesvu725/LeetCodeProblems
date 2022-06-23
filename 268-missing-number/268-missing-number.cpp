309288b71a8b5f1ecbb23d8110b0ed5700db1b2c
jamesvu725 <35977136+jamesvu725@users.noreply.github.com>
2022-06-22 17:31:48 -0700

    Time: 24 ms (70.48%), Space: 17.9 MB (64.12%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // naive approach
-        // sort and find first number
+        // sorting approach
+        // time complexity: O(nlogn), space complexity: O(1)
         // sort(nums.begin(), nums.end());
         // for (int i = 0; i < nums.size(); ++i) {
+        // return early if missing number found
         //     if (nums[i] != i) {
         //         return i;
         //     }
         // }
-        // return size since all numbers are present between 0 and n-1
+        // return size since all numbers are present between 0 and size - 1
         // return nums.size();
         
-        // with extra memory
+        // with extra vector
+        // time complexity: O(n), space complexity: O(n)
         // create a boolean vector of size + 1
-        vector<bool> v(nums.size() + 1, false);
+        // vector<bool> v(nums.size() + 1, false);
         // flip the boolean value for each number in nums
-        for (int i = 0; i < nums.size(); ++i) {
-            v[nums[i]] = true;
+        // for (int i = 0; i < nums.size(); ++i) {
+        //     v[nums[i]] = true;
+        // }
+        // return if false is found
+        // for (int i = 0; i < v.size(); ++i) {
+        //     if (v[i] == false) {
+        //         return i;
+        //     }
+        // }
+        // should never reach, -1 if error
+        // return -1;
+        
+        // math approach
+        int sum = 0;
+        // total is the summation from 0 to size
+        int total = nums.size() * (nums.size() + 1) / 2;
+        // add up sum from nums
+        for (int i: nums) {
+            sum += i;
         }
-        for (int i = 0; i < v.size(); ++i) {
-            if (v[i] == false) {
-                return i;
-            }
-        }
-        return -1;
+        // answer is missing number from total and sum
+        return total - sum;
     }
 };
